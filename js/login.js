7f5b44176a657a7f8630d6ff8b67596135c0f749
Samuel Kim <kbeom00@gmail.com>
2021-10-01 23:12:08 +0900

    Created login.js

---
+++ js/login.js
@@ -0,0 +1,31 @@
+const inputBox = document.getElementById("input-box");
+const inputForm = document.getElementById("input-form");
+const submitBtn = document.getElementById("submit-button");
+
+const greeting = document.getElementById("greeting");
+
+let usrName = localStorage.getItem("usrname");
+
+function saveUsrName(event) {
+  event.preventDefault();
+  localStorage.setItem("usrname", inputForm.value);
+  usrName = inputForm.value;
+  hideInputShowGreeting();
+  createGreeting();
+}
+
+function hideInputShowGreeting() {
+  inputBox.classList.add("hidden");
+  greeting.classList.remove("hidden");
+}
+
+function createGreeting() {
+  greeting.innerText = `Have a productive day ${usrName}!`;
+}
+
+if (usrName === null) {
+  submitBtn.addEventListener("click", saveUsrName);
+} else {
+  hideInputShowGreeting();
+  createGreeting();
+}
