5765ccb6e1ee260087c80e90ede1af3fb8d98744
Samuel Kim <kbeom00@gmail.com>
2021-10-02 22:59:06 +0900

    Created slider.js, not completed.

---
+++ js/slider.js
@@ -0,0 +1,4 @@
+const dayBtn = document.getElementById("day-btn");
+const weekBtn = document.getElementById("week-btn");
+const monthBtn = document.getElementById("month-btn");
+const yearBtn = document.getElementById("year-btn");
